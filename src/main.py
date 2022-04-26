43e565a75f24be14d8f415e6a3af29db4f169eb1
AlexVulpoiu <vulpoiu_alexandru@yahoo.com>
2022-04-26 17:29:51 +0300

    add initial project

---
+++ src/main.py
@@ -0,0 +1,30 @@
+def function(sir, character, no):
+    global max_len
+
+    if len(sir) > max_len:
+        raise Exception('String length too big')
+    if no <= 1:
+        raise Exception('Invalid number')
+    if not character.islower():
+        raise Exception('Invalid character')
+
+    apparitions = 0
+    for c in sir:
+        if c == character:
+            apparitions += 1
+    if no > apparitions:
+        lower = apparitions
+    else:
+        lower = no
+
+    div = 2
+    while div <= lower:
+        if apparitions % div == 0 and no % div == 0:
+            return False
+        div += 1
+
+    return True
+
+
+if __name__ == '__main__':
+    max_len = 50
