254ebc0aea23079ec963ba8deb1e7f69b1300b06
dianagrigore <diana.grigore12@yahoo.com>
2022-04-27 19:08:41 +0300

    fixed bug in exception testing

--- test/test_structural.py
+++ test/test_structural.py
@@ -6,40 +6,40 @@ class MyTestCase(unittest.TestCase):
     def test_conditional_coverage(self):
         with self.assertRaises(Exception) as context:
             coprime_apparitions('abcdefghijklmnoprabcdefghijklmnoprabcdefghijklmnopr', 't', 2)
-            self.assertEqual(context.exception.args[0], Exception('Empty string or string length too big').args[0])
+            self.assertEqual(context.exception.args[0], 'Empty string or string length too big')
 
         with self.assertRaises(Exception) as context:
             coprime_apparitions('test', 't', -2)
-            self.assertEqual(context.exception.args[0], Exception('Invalid number').args[0])
+            self.assertEqual(context.exception.args[0], 'Invalid number')
 
         with self.assertRaises(Exception) as context:
             coprime_apparitions('test', '1', 2)
-            self.assertEqual(context.exception.args[0], Exception('Invalid character').args[0])
+            self.assertEqual(context.exception.args[0], 'Invalid character')
 
         with self.assertRaises(Exception) as context:
             coprime_apparitions('test', 'm', 2)
-            self.assertEqual(context.exception.args[0], Exception('0 apparitions').args[0])
+            self.assertEqual(context.exception.args[0], '0 apparitions')
 
         with self.assertRaises(Exception) as context:
             coprime_apparitions('', 'm', 2)
-            self.assertEqual(context.exception.args[0], Exception('Empty string or string length too big').args[0])
+            self.assertEqual(context.exception.args[0], 'Empty string or string length too big')
 
     def test_branch_coverage(self):
         with self.assertRaises(Exception) as context:
             coprime_apparitions('', 'A', 2)
-            self.assertEqual(context.exception.args[0], Exception('Empty string or string length too big').args[0])
+            self.assertEqual(context.exception.args[0], 'Empty string or string length too big')
 
         with self.assertRaises(Exception) as context:
             coprime_apparitions('merge', 'A', 0)
-            self.assertEqual(context.exception.args[0], Exception('Invalid number').args[0])
+            self.assertEqual(context.exception.args[0], 'Invalid number')
 
         with self.assertRaises(Exception) as context:
             coprime_apparitions('merge', 'A', 1)
-            self.assertEqual(context.exception.args[0], Exception('Invalid character').args[0])
+            self.assertEqual(context.exception.args[0], 'Invalid character')
 
         with self.assertRaises(Exception) as context:
             coprime_apparitions('merge', 'a', 1)
-            self.assertEqual(context.exception.args[0], Exception('Empty string or string length too big').args[0])
+            self.assertEqual(context.exception.args[0], '0 apparitions')
 
         self.assertEqual(coprime_apparitions('merge', 'e', 2), False)
         self.assertEqual(coprime_apparitions('merge', 'e', 3), True)
