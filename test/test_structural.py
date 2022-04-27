823783364809904b52d46511e34babef675a5494
Delia-Gherghe <61266187+Delia-Gherghe@users.noreply.github.com>
2022-04-27 16:35:49 +0300

    added statement coverage tests

--- test/test_structural.py
+++ test/test_structural.py
@@ -44,6 +44,27 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(coprime_apparitions('merge', 'e', 2), False)
         self.assertEqual(coprime_apparitions('merge', 'e', 3), True)
 
+    def test_statement_coverage(self):
+        with self.assertRaises(Exception) as context:
+            coprime_apparitions("", 'a', 3)
+        self.assertEqual(context.exception.args[0], 'Empty string or string length too big')
+
+        with self.assertRaises(Exception) as context:
+            coprime_apparitions("abc", 'a', 0)
+        self.assertEqual(context.exception.args[0], 'Invalid number')
+
+        with self.assertRaises(Exception) as context:
+            coprime_apparitions("abc", 'A', 2)
+        self.assertEqual(context.exception.args[0], 'Invalid character')
+
+        with self.assertRaises(Exception) as context:
+            coprime_apparitions("a", 'f', 5)
+        self.assertEqual(context.exception.args[0], '0 apparitions')
+
+        self.assertFalse(coprime_apparitions("aab", 'a', 4))
+
+        self.assertTrue(coprime_apparitions("aaaab", 'a', 3))
+
     def test_valid_cases(self):
         self.assertEqual(coprime_apparitions('test', 't', 4), False)
         self.assertEqual(coprime_apparitions('test', 't', 3), True)
